228eb51f235cc7918f3c8f8d80f62d227d77e3c2
Virginia Thayer <virginia@stripe.com>
2019-06-19 16:14:43 -0700

    stretch: added GSAP animation to menu click

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,7 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open')
+    TweenMax.from(menu, 1, {x:-50});
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -10,4 +11,4 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
-menuButton.addEventListener('click', () => toggleMenu())
+menuButton.addEventListener('click', toggleMenu)
